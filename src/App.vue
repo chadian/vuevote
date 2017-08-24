3a9ac21f95c751d4af009e5e6d6e4e04e9279975
Chad Carbert <chadcarbert@me.com>
2017-08-24 18:15:30 +0200

    Initial commit with basic vote-question component

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>Welcome to vuevote</h1>
+    <vote-question :question="question" :choices="choices"></vote-question>
+  </div>
+</template>
+
+<script>
+import VoteQuestion from './vote-question.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      question: 'Who should be President?',
+      choices: [
+        { id: '0', name: 'Bernie Sanders', },
+        { id: '1', name: 'Hiliary Clinton', },
+        { id: '2', name: 'Orange Clown' },
+      ]
+    }
+  },
+  components: {
+    VoteQuestion
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/vote-question.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="question">
+    {{question}}
+    <ul class="choices">
+      <li v-for="choice in choices" v-on:click="choose(choice)">
+        {{choice.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['question', 'choices'],
+  methods: {
+    choose: function(choice) {
+      alert(`You voted for ${choice.name}`);
+    }
+  }
+}
+</script>
