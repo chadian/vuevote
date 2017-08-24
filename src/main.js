9d5fb2d73e2e74d220b6ed279d7d26588ea20e48
Chad Carbert <chadcarbert@me.com>
2017-08-24 22:13:09 +0200

    Use router, and pull question and choices through dynamic segment json data

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Vote from './vote.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/:token', component: Vote }
+];
+
+const router = new VueRouter({ routes })
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  router,
+}).$mount('#app');
+
+const voteData = {
+  question: 'Who should be President?',
+  choices: [
+    { id: '0', name: 'Bernie Sanders', },
+    { id: '1', name: 'Hiliary Clinton', },
+    { id: '2', name: 'Orange Clown' },
+  ]
+};
+
+const voteDataEncoded = window.encodeURIComponent(JSON.stringify(voteData));
+router.push(`/${voteDataEncoded}`);
---
+++ src/vote.vue
@@ -0,0 +1,24 @@
+<template>
+  <vote-question
+    v-on:choiceMade="choiceMade"
+    :question="question"
+    :choices="choices"></vote-question>
+</template>
+
+<script>
+import VoteQuestion from './vote-question.vue';
+export default {
+  name: 'app',
+  data() {
+    return JSON.parse(this.$route.params.token);
+  },
+  components: {
+    VoteQuestion
+  },
+  methods: {
+    choiceMade(choice) {
+      alert(`You have chosen ${choice.name}`);
+    }
+  }
+}
+</script>
